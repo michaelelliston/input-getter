2639850f4f23750e6a74de4ac7a85c888cae334e
Michael <michael.elliston95@gmail.com>
2025-10-29 15:52:43 -0500

    getString, getInt, and getFloat added with functionality - untested

--- src/main/java/utilities/InputGetter.java
+++ src/main/java/utilities/InputGetter.java
@@ -1,4 +1,32 @@
 package utilities;
 
+import java.util.Scanner;
+
 public class InputGetter {
+    private static final Scanner myScanner = new Scanner(System.in);
+
+    public static String getString(String prompt) {
+        System.out.println(prompt);
+        return myScanner.nextLine().trim();
+    }
+
+    public static int getInt(String prompt) {
+        System.out.println(prompt);
+        try {
+            return Integer.parseInt(myScanner.nextLine().trim());
+        } catch (NumberFormatException e) {
+            System.err.println("An error occurred while parsing your input: " + e);
+            return 0;
+        }
+    }
+
+    public static float getFloat(String prompt) {
+        System.out.println(prompt);
+        try {
+            return Float.parseFloat(myScanner.nextLine().trim());
+        } catch (NumberFormatException e) {
+            System.err.println("An error occurred while parsing your input: " + e);
+            return 0;
+        }
+    }
 }
