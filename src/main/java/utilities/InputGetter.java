abebc911b8df1aa5cde3a353338bd9f1021980d4
Michael <michael.elliston95@gmail.com>
2025-10-30 09:10:13 -0500

    finished creating getValue methods, and modified them to loop until a value is returned.

--- src/main/java/utilities/InputGetter.java
+++ src/main/java/utilities/InputGetter.java
@@ -11,22 +11,60 @@ public class InputGetter {
     }
 
     public static int getInt(String prompt) {
-        System.out.println(prompt);
-        try {
-            return Integer.parseInt(myScanner.nextLine().trim());
-        } catch (NumberFormatException e) {
-            System.err.println("An error occurred while parsing your input: " + e);
-            return 0;
+        while (true) {
+            System.out.println(prompt);
+            try {
+                return Integer.parseInt(myScanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid input. Please only enter the number values you wish to input (ex: 73).\n");
+            }
         }
     }
 
     public static float getFloat(String prompt) {
-        System.out.println(prompt);
-        try {
-            return Float.parseFloat(myScanner.nextLine().trim());
-        } catch (NumberFormatException e) {
-            System.err.println("An error occurred while parsing your input: " + e);
-            return 0;
+        while (true) {
+            System.out.println(prompt);
+            try {
+                return Float.parseFloat(myScanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid input. Please only enter the number and decimal values you wish to input (ex: 3.14).\n");
+            }
+        }
+    }
+
+    public static double getDouble(String prompt) {
+        while (true) {
+            System.out.println(prompt);
+            try {
+                return Double.parseDouble(myScanner.nextLine().trim());
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid input. Please only enter the number and decimal values you wish to input (ex: 73 or 3.14).\n");
+            }
+        }
+    }
+
+    public static char getChar(String prompt) {
+        while (true) {
+            System.out.println(prompt);
+            try {
+                return myScanner.nextLine().trim().charAt(0);
+            } catch (IndexOutOfBoundsException e) {
+                System.err.println("Invalid input. Please enter only a single character.\n");
+            }
+        }
+    }
+
+    public static boolean getBoolean(String prompt) {
+        while (true) {
+            System.out.println(prompt);
+            String userInput = myScanner.nextLine().trim().toLowerCase();
+            if (userInput.equals("true") || userInput.equals("t") || userInput.equals("yes") || userInput.equals("y")) {
+                return true;
+            } else if (userInput.equals("false") || userInput.equals("f") || userInput.equals("no") || userInput.equals("n")) {
+                return false;
+            } else {
+                System.err.println("Invalid input; please enter a valid response (true / false).\n");
+            }
         }
     }
 }
